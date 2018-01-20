e8c118a22a10a45910f0928af64bb488da521796
Daniel Stanciu <danielstanciu91@gmail.com>
2018-01-20 12:25:07 +0000

    Add comments to the GameStateManager class to make the code easier to read

--- core/src/daniel/stanciu/flappybird/states/GameStateManager.java
+++ core/src/daniel/stanciu/flappybird/states/GameStateManager.java
@@ -12,23 +12,28 @@ public class GameStateManager {
         states = new Stack<State>();
     }
 
+    //Pushes a state onto the top oft he stack
     public void push(State state) {
         states.push(state);
     }
 
+    //Removes the state at the top of the stack
     public void pop() {
         states.pop();
     }
 
+    //Replaces the top state with another state
     public void set(State state) {
         states.pop();
         states.push(state);
     }
 
+    //Look at the top state of the stack and change the delta time (changing time between renders)
     public void update(float dt) {
         states.peek().update(dt);
     }
 
+    //Looks at the top state of the stack and renders thee sprite batch (container of all graphics)
     public void render(SpriteBatch sb) {
         states.peek().render(sb);
     }
