47d20093485eb5fb9169cc680db59273515d0c1f
Daniel Stanciu <danielstanciu91@gmail.com>
2018-01-20 12:05:58 +0000

    Set the desktop screen size

--- desktop/src/daniel/stanciu/flappybird/desktop/DesktopLauncher.java
+++ desktop/src/daniel/stanciu/flappybird/desktop/DesktopLauncher.java
@@ -7,6 +7,12 @@ import daniel.stanciu.flappybird.FlappyBird;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+
+		//Set the window size and the title of the desktop version
+		config.width = FlappyBird.WIDTH;
+		config.height = FlappyBird.HEIGHT;
+		config.title = FlappyBird.TITLE;
+
 		new LwjglApplication(new FlappyBird(), config);
 	}
 }
